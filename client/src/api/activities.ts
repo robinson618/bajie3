dea19e71b6c78730bd4680877f00a0ccc91f44ab
robinson618 <69332204+robinson618@users.noreply.github.com>
2026-06-08 17:30:07 +0800

    Delete client directory

--- client/src/api/activities.ts
+++
@@ -1,8 +0,0 @@
-import request from './request';
-
-export const activitiesApi = {
-  getList: (page = 1, pageSize = 10) =>
-    request.get('/activities', { params: { page, pageSize } }),
-  getDetail: (id: number) =>
-    request.get(`/activities/${id}`),
-};
