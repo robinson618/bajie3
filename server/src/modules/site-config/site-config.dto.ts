a1970aab75ec751264dd1bfa78b03a24c55d8888
robinson618 <69332204+robinson618@users.noreply.github.com>
2026-06-08 17:30:37 +0800

    Delete server directory

--- server/src/modules/site-config/site-config.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateSiteConfigDto {
-  @ApiProperty() @IsString() @IsNotEmpty() configKey: string;
-  @ApiProperty() @IsString() @IsNotEmpty() configValue: string;
-  @ApiProperty() @IsString() @IsOptional() configType?: string;
-}
